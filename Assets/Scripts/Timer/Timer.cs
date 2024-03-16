25d2c979add4871040b77d8f7cf903f73f3d3604
Léo Simon <leoalex.simon@gmail.com>
2024-03-16 19:20:32 +0100

    Test de merde sur la pause du timer

--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -11,6 +11,7 @@ public class Timer : MonoBehaviour
     [SerializeField] float CurrentTime = 10f;
     [SerializeField] TextMeshProUGUI countdownText;
     public Timer_test JeSuisDansUnAutreScript_Test;
+    bool stopTime = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,26 +22,45 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FreezTime();
         timer();
 
     }
     void timer()
     {
-        CurrentTime -= 1 * Time.deltaTime;
-        countdownText.text = CurrentTime.ToString("0");
-
-        if (CurrentTime <= 0)
+        if (!stopTime)
         {
-            CurrentTime = 0;
-            JeSuisDansUnAutreScript_Test.JeSuisDansUnAutreScript();
-            EndGame();
+            CurrentTime -= 1 * Time.deltaTime;
+            countdownText.text = CurrentTime.ToString("0");
+
+            if (CurrentTime <= 0)
+            {
+                CurrentTime = 0;
+                JeSuisDansUnAutreScript_Test.JeSuisDansUnAutreScript();
+                EndGame();
+            }
+
         }
-        if (Input.GetKeyDown(KeyCode.Escape)) {
 
-            
+       
+    }
+    public void FreezTime()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("je freez");
+            stopTime = true;
 
 
         }
+        if (Input.GetKeyDown(KeyCode.E)&&stopTime == true)
+        {
+            stopTime = false;
+
+
+        }
+
     }
     void EndGame()
     {
