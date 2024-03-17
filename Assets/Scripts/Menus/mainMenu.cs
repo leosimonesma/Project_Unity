f57526a1f4e6c94af434b4970691e1caebbdd1e4
Baguette555 <Maxoulol124@gmail.com>
2024-03-17 15:47:08 +0100

    Main Menu
    Menu principal ajouté

---
+++ Assets/Scripts/Menus/mainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene("SCN_TestRayCast");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Jeu quitté");
+        Application.Quit();
+    }
+}
