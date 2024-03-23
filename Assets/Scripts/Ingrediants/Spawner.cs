a635760ae28bfcb3b08e19d141d22c8a4115969d
Léo Simon <leoalex.simon@gmail.com>
2024-03-23 15:41:39 +0100

    spawner

--- Assets/Scripts/Ingrediants/Spawner.cs
+++ Assets/Scripts/Ingrediants/Spawner.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] GameObject Objet_01;
-    [SerializeField] GameObject Objet_02;
-    [SerializeField] GameObject Objet_03;
-    [SerializeField] GameObject Objet_04;
-    [SerializeField] GameObject Objet_05;
-    [SerializeField] GameObject Objet_06;
-    [SerializeField] GameObject Objet_07;
-    [SerializeField] GameObject Objet_08;
-    [SerializeField] GameObject Objet_09;
-    [SerializeField] GameObject Objet_10;
+    [SerializeField] GameObject Ingredient_01;
+    [SerializeField] GameObject Ingredient_02;
+    [SerializeField] GameObject Ingredient_03;
+    [SerializeField] GameObject Ingredient_04;
+    [SerializeField] GameObject Ingredient_05;
+    [SerializeField] GameObject Ingredient_06;
+    [SerializeField] GameObject Ingredient_07;
+    [SerializeField] GameObject Ingredient_08;
+    [SerializeField] GameObject Ingredient_09;
+    [SerializeField] GameObject Ingredient_10;
+
+    [SerializeField] float spawner_Interval = 5f;
 
 
 
@@ -21,12 +23,46 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        StartCoroutine(spawnItems(spawner_Interval, SelecNextIngredient()));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator spawnItems(float interval, GameObject ingredient )
     {
-        
+        yield return new WaitForSeconds( interval );
+        GameObject newIngredient = Instantiate( ingredient, new Vector3(Random.Range(-1f,1f), Random.Range(-2f,2f), 0f), Quaternion.identity);
+        StartCoroutine(spawnItems(interval, SelecNextIngredient()));
+
+    }
+    private GameObject SelecNextIngredient()
+    {
+        int _random = UnityEngine.Random.Range(1, 11);
+        switch (_random)
+        {
+            case 1:
+                return Ingredient_01;
+
+            case 2:
+                return Ingredient_02;
+            case 3:
+                return Ingredient_03;
+            case 4:
+                return Ingredient_04;
+            case 5:
+                return Ingredient_05;
+            case 6:
+                return Ingredient_06;
+            case 7:
+                return Ingredient_07;
+            case 8:
+                return Ingredient_08;
+            case 9:
+                return Ingredient_09;
+            case 10:
+                return Ingredient_10;
+               
+            default: return null;
+
+        }
     }
 }
